d282b4266785d3bd156235e73ddd66f003465292
AnimMouse <git@animmouse.com>
2023-08-31 00:39:07 +0800

    feat: Add other OS on test workflow and autostart on Windows

--- scripts/download/Windows.ps1
+++ scripts/download/Windows.ps1
@@ -2,5 +2,6 @@ $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 Set-StrictMode -Version Latest
 Write-Host ::group::Downloading cloudflared $env:version for Windows
+New-Item cloudflared -ItemType Directory -Force
 Invoke-WebRequest $env:GITHUB_SERVER_URL/cloudflare/cloudflared/releases/download/$env:version/cloudflared-windows-amd64.exe -OutFile cloudflared\cloudflared.exe
 Write-Host ::endgroup::
