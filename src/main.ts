71cd008a9a284190e0332e104dd346c8939695da
jcbhmr <jcbhmr@outlook.com>
2024-01-24 15:14:04 -0600

    macos

--- src/main.ts
+++ src/main.ts
@@ -38,25 +38,35 @@ if (!version) throw new DOMException(`${versionRaw} resolved to ${version}`);
 let found = tc.find("cloudflared", version);
 core.setOutput("cache-hit", !!found);
 if (!found) {
-  const target = {
-    "linux,arm64": "linux-arm64",
-    "linux,x64": "linux-amd64",
-    "win32,x64": "windows-amd64",
-  }[[process.platform, process.arch].toString()]!;
-  const exeExt = {
-    darwin: "",
-    linux: "",
-    win32: ".exe",
-  }[process.platform.toString()]!;
-  const file = `cloudflared-${target}${exeExt}`;
+  if (process.platform === "darwin") {
+    const archive = "cloudflared-darwin-amd64.tgz"
+    found = await tc.downloadTool(
+      `https://github.com/cloudflare/cloudflared/releases/download/${version}/${archive}`,
+    );
+    found = await tc.extractTar(found);
+    // 'cloudflared' binary is in root of archive. No 'join()' needed.
+  } else {
+    const target = {
+      "linux,arm64": "linux-arm64",
+      "linux,x64": "linux-amd64",
+      "win32,x64": "windows-amd64",
+    }[[process.platform, process.arch].toString()]!;
+    const exeExt = {
+      darwin: "",
+      linux: "",
+      win32: ".exe",
+    }[process.platform.toString()]!;
+    const file = `cloudflared-${target}${exeExt}`;
 
-  found = await tc.downloadTool(
-    `https://github.com/cloudflare/cloudflared/releases/download/${version}/${file}`,
-  );
-  await mkdir(`${found}-folder`);
-  await rename(found, join(`${found}-folder`, "cloudflared"));
-  found = `${found}-folder`;
-  await chmod(join(found, "cloudflared"), 0o775);
+    found = await tc.downloadTool(
+      `https://github.com/cloudflare/cloudflared/releases/download/${version}/${file}`,
+    );
+    await chmod(found, 0o775);
+    await mkdir(`${found}-folder`);
+    await rename(found, join(`${found}-folder`, "cloudflared"));
+    found = `${found}-folder`;
+  }
+
   found = await tc.cacheDir(found, "cloudflared", version);
   core.info(`cloudflared v${version} added to cache`);
 }
