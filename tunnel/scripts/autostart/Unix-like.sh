fa1621af7231e5357a9e70d42bddbe1a480cd795
Anim Mouse <git@animmouse.com>
2024-09-19 17:12:55 +0800

    fix: Fix TryCloudflare command line arguments

--- tunnel/scripts/autostart/Unix-like.sh
+++ tunnel/scripts/autostart/Unix-like.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -eu
 echo ::group::Autostarting cloudflared tunnel
-nohup cloudflared --pidfile $RUNNER_TEMP/cloudflared.pid --logfile $RUNNER_TEMP/cloudflared.log tunnel run $(if [ "$url" != false ]; then echo "--url "$url""; fi) &
+if [ "$url" = false ]
+then
+  nohup cloudflared --pidfile $RUNNER_TEMP/cloudflared.pid --logfile $RUNNER_TEMP/cloudflared.log tunnel run &
+else
+  nohup cloudflared --pidfile $RUNNER_TEMP/cloudflared.pid --logfile $RUNNER_TEMP/cloudflared.log tunnel --url "$url" &
+fi
 echo ::endgroup::
--- tunnel/scripts/autostart/Windows.ps1
+++ tunnel/scripts/autostart/Windows.ps1
@@ -1,5 +1,10 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 Write-Host ::group::Autostarting cloudflared for Windows
-Start-Process "cloudflared" "--pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel run $(if ($env:url -ne 'false') {Write-Output "--url "$env:url""})"
+if ($env:url -eq 'false') {
+  Start-Process "cloudflared" "--pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel run"
+}
+else {
+  Start-Process "cloudflared" "--pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel --url $env:url"
+}
 Write-Host ::endgroup::
