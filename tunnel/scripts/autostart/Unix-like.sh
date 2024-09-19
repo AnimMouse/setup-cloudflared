ae4f422d9c990e3117a6a22ce280b7f2e7c784ff
Anim Mouse <git@animmouse.com>
2024-09-19 16:22:40 +0800

    feat: Add TryCloudflare feature

--- tunnel/scripts/autostart/Unix-like.sh
+++ tunnel/scripts/autostart/Unix-like.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
-echo ::group::Autostarting cloudflared for $RUNNER_OS
-nohup cloudflared --pidfile $RUNNER_TEMP/cloudflared.pid --logfile $RUNNER_TEMP/cloudflared.log tunnel run &
+echo ::group::Autostarting cloudflared tunnel
+nohup cloudflared --pidfile $RUNNER_TEMP/cloudflared.pid --logfile $RUNNER_TEMP/cloudflared.log tunnel run $(if [ "$url" != false ]; then echo "--url "$url""; fi) &
 echo ::endgroup::
--- tunnel/scripts/autostart/Windows.ps1
+++ tunnel/scripts/autostart/Windows.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 Write-Host ::group::Autostarting cloudflared for Windows
-Start-Process "cloudflared" "--pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel run"
+Start-Process "cloudflared" "--pidfile $env:RUNNER_TEMP/cloudflared.pid --logfile $env:RUNNER_TEMP/cloudflared.log tunnel run $(if ($env:url -ne 'false') {Write-Output "--url "$env:url""})"
 Write-Host ::endgroup::
